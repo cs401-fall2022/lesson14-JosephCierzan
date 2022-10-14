ab01c6498c4eba55f7c8a2ca290d540de0210289
Joseph Cierzan <77242731+JosephCierzan@users.noreply.github.com>
2022-10-13 19:44:09 -0600

    Test code

--- app.ts
+++ app.ts
@@ -6,4 +6,15 @@ function hello (): String {
     return "Hello World";
 };
 
-export {hello}
+function turnRed() {
+    if(document != null){
+        let c = document.getElementById("demo").style.color;
+        if(c === 'red'){
+            document.getElementById("demo").style.color = "black";
+        }else{
+            document.getElementById("demo").style.color = "red";
+        }
+    }    
+}
+
+export {hello, turnRed}
