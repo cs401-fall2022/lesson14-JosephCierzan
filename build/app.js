8177d0be3030b398f9f1dfd5b9969e8dd07bc4f7
Joseph Cierzan <josephcierzan@u.boisestate.edu>
2022-10-20 19:12:03 -0600

    Files to run

---
+++ build/app.js
@@ -0,0 +1,11 @@
+/**
+ * This returns the string hello
+ * @returns the String hello
+ */
+function hello() {
+    return "Hello World";
+}
+;
+export { hello };
+console.log(hello());
+//# sourceMappingURL=app.js.map
