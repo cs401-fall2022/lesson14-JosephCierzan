afe7d991bcd5258729e7bf192234148cce10b09e
Joseph Cierzan <josephcierzan@u.boisestate.edu>
2022-10-20 18:46:37 -0600

    files for TS/JS

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.hello = void 0;
+/**
+ * This returns the string hello
+ * @returns the String hello
+ */
+function hello() {
+    return "Hello World";
+}
+exports.hello = hello;
+
+console.log(hello());
--- app.ts
+++ app.ts
@@ -2,19 +2,10 @@
  * This returns the string hello
  * @returns the String hello
  */
-function hello (): String {
+ function hello(): String {
     return "Hello World";
 };
 
-function turnRed() {
-    if(document != null){
-        let c = document.getElementById("demo").style.color;
-        if(c === 'red'){
-            document.getElementById("demo").style.color = "black";
-        }else{
-            document.getElementById("demo").style.color = "red";
-        }
-    }    
-}
 
-export {hello, turnRed}
+export {hello};
+console.log(hello());
